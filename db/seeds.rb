c454b9ecad69b02b472345fe2a23b6c2133dba99
amangoutam <goutam.aman@gmail.com>
2021-10-05 16:44:50 +0530

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+    {
+        name: "Test 1",
+        brand: "brand 1",
+        price: "price 1",
+        description: "desc 1"
+    },
+    {
+        name: "Test 2",
+        brand: "brand 2",
+        price: "price 2",
+        description: "desc 2"
+    },
+    {
+        name: "Test 3",
+        brand: "brand 3",
+        price: "price 3",
+        description: "desc 3"
+    },
+    {
+        name: "Test 4",
+        brand: "brand 4",
+        price: "price 4",
+        description: "desc 4"
+    },
+    {
+        name: "Test 5",
+        brand: "brand 5",
+        price: "price 5",
+        description: "desc 5"
+    },
+    {
+        name: "Test 6",
+        brand: "brand 6",
+        price: "price 6",
+        description: "desc 6"
+    },
+])
